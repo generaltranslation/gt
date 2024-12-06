12ef55683a2ce44eff12bd67cfa588cb74dc4186
archie-mckenzie <archie@generaltranslation.com>
2024-12-06 12:02:05 -0800

    added additional languages

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,21 @@
+/**
+ * @function getSupportedLocale
+ * @description
+ * Takes an arbitrary locale string, validates and standardizes it, and then attempts to map it
+ * to a supported locale code based on a predefined list of locales. If the exact locale is supported,
+ * it returns that locale directly. Otherwise, it attempts to find a compatible fallback by:
+ *   1. Checking if the language portion is supported.
+ *   2. Checking if a minimized form (e.g. "en" for "en-US") is supported.
+ * If no supported match is found, it returns null.
+ *
+ * @param {string} locale - The locale string to check (e.g., "en-Latn-US").
+ * @returns {string | null} A valid supported locale code if matched, otherwise null.
+ */
 declare function getSupportedLocale(locale: string): string | null;
+/**
+ * Generates a sorted list of supported locales.
+ * @returns {string[]} A sorted array containing the supported base languages and their specific locales.
+ */
+declare function listSupportedLocales(): string[];
 
-export { getSupportedLocale };
+export { getSupportedLocale, listSupportedLocales };
---
+++ src/supportedLocales.ts
@@ -0,0 +1,127 @@
+const supportedLocales = {
+    "am": {}, // Amharic
+    "ar": { // Arabic
+        "ar-AE": true, // United Arab Emirates
+        "ar-EG": true, // Egypt
+        "ar-LB": true, // Lebanon
+        "ar-MA": true, // Morocco
+        "ar-SA": true, // Saudi Arabia
+    },
+    "bg": {}, // Bulgarian
+    "bn": { // Bengali
+        "bn-BD": true, // Bangladesh
+        "bn-IN": true, // India
+    },
+    "bs": {}, // Bosnian
+    "ca": {}, // Catalan
+    "cs": {}, // Czech
+    "da": {}, // Danish
+    "de": { // German
+        "de-AT": true, // Austria
+        "de-CH": true, // Switzerland
+        "de-DE": true, // Germany
+    },
+    "el": { // Greek
+        "el-CY": true, // Cyprus
+        "el-GR": true, // Greece
+    },
+    "en": { // English
+        "en-AU": true, // Australia
+        "en-CA": true, // Canada
+        "en-GB": true, // United Kingdom
+        "en-NZ": true, // New Zealand
+        "en-US": true, // United States
+    },
+    "es": { // Spanish
+        "es-419": true, // Latin America
+        "es-AR": true, // Argentina
+        "es-CL": true, // Chile
+        "es-CO": true, // Colombia
+        "es-ES": true, // Spain
+        "es-MX": true, // Mexico
+        "es-PE": true, // Peru
+        "es-US": true, // United States
+        "es-VE": true, // Venezuela
+    },
+    "et": {}, // Estonian
+    "fa": {}, // Persian
+    "fi": {}, // Finnish
+    "fr": { // French
+        "fr-BE": true, // Belgium
+        "fr-CM": true, // Cameroon
+        "fr-CA": true, // Canada
+        "fr-CH": true, // Switzerland
+        "fr-FR": true, // France
+        "fr-SN": true, // Senegal
+    },
+    "gu": {}, // Gujarati
+    "hi": {}, // Hindi
+    "he": {}, // Hebrew
+    "hr": {}, // Croatian
+    "hu": {}, // Hungarian
+    "hy": {}, // Armenian
+    "id": {}, // Indonesian
+    "is": {}, // Icelandic
+    "it": { // Italian
+        "it-CH": true, // Switzerland
+        "it-IT": true, // Italy
+    },
+    "ja": {}, // Japanese
+    "ka": {}, // Georgian
+    "kk": {}, // Kazakh
+    "kn": {}, // Kannada
+    "ko": {}, // Korean
+    "la": {}, // Latin
+    "lt": {}, // Lithuanian
+    "lv": {}, // Latvian
+    "mk": {}, // Macedonian
+    "ml": {}, // Malayalam
+    "mn": {}, // Mongolian
+    "mr": {}, // Marathi
+    "ms": {}, // Malay
+    "my": {}, // Burmese
+    "nl": { // Dutch
+        "nl-BE": true, // Belgium
+        "nl-NL": true, // Netherlands
+    },
+    "no": {}, // Norwegian
+    "pa": {}, // Punjabi
+    "pl": {}, // Polish
+    "pt": { // Portuguese
+        "pt-BR": true, // Brazil
+        "pt-PT": true, // Portugal
+    },
+    "ro": {}, // Romanian
+    "ru": {}, // Russian
+    "sk": {}, // Slovak
+    "sl": {}, // Slovenian
+    "so": {}, // Somali
+    "sq": {}, // Albanian
+    "sr": {}, // Serbian
+    "sv": {}, // Swedish
+    "sw": { // Swahili
+        "sw-KE": true, // Kenya
+        "sw-TZ": true, // Tanzania
+    },
+    "ta": {}, // Tamil
+    "te": {}, // Telugu
+    "th": {}, // Thai
+    "tl": {}, // Tagalog
+    "tlh": {}, // Klingon
+    "tr": {}, // Turkish
+    "uk": {}, // Ukrainian
+    "ur": {}, // Urdu
+    "vi": {}, // Vietnamese
+    "zh": { // Chinese
+        "zh-CN": true, // China
+        "zh-HK": true, // Hong Kong
+        "zh-SG": true, // Singapore
+        "zh-TW": true, // Taiwan
+    },
+} as {
+    [language: string]: {
+        [locale: string]: true
+    }
+};
+
+export default supportedLocales;
