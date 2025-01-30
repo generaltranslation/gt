155aa2b21443b2237024382a2ea8f534a19bfc08
ErnestM1234 <ernestm@princeton.edu>
2025-01-29 16:40:41 -0800

    build react

--- packages/react/dist/provider/config/defaultProps.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.batchInterval = exports.maxBatchSize = exports.maxConcurrentRequests = void 0;
-exports.maxConcurrentRequests = 100;
-exports.maxBatchSize = 25;
-exports.batchInterval = 50;
-//# sourceMappingURL=defaultProps.js.map
