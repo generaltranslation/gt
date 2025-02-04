24b3e04705e2f2f3b7f488226e9c29aa65267731
Ernest McCarter <ernest@generaltranslation.com>
2025-02-03 17:02:37 -0800

    Em/env reader (#52)
    * add support for reading from env

--- packages/next/dist/errors/createErrors.d.ts
+++ packages/next/dist/errors/createErrors.d.ts
@@ -1,11 +1,10 @@
 export declare const projectIdMissingError = "General Translation: Project ID missing! Set projectId as GT_PROJECT_ID in the environment or by passing the projectId parameter to initGT(). Find your project ID: www.generaltranslation.com/dashboard.";
 export declare const APIKeyMissingError = "General Translation: API key is required for runtime translation! Create an API key: www.generaltranslation.com/dashboard/api-keys. (Or, turn off runtime translation by setting runtimeUrl to an empty string.)";
 export declare const remoteTranslationsError = "General Translation: Error fetching remote translation.";
-export declare const renderingError = "General Translation: Rendering error.";
 export declare const createStringTranslationError: (content: string, id?: string) => string;
 export declare const createDictionaryStringTranslationError: (id: string) => string;
 export declare const createRequiredPrefixError: (id: string, requiredPrefix: string) => string;
-export declare const devApiKeyIncludedInProductionError = "General Translation: You are attempting a production build of your app with a developer API key (beginning \"gtx-dev-\"). Replace this API key with a production API key (beginning \"gtx-api-\") when you build your app for production.";
+export declare const devApiKeyIncludedInProductionError = "General Translation: You are attempting a production using a development API key. Replace this API key with a production API key when you build your app for production.";
 export declare const createDictionarySubsetError: (id: string, functionName: string) => string;
 export declare const usingDefaultsWarning = "General Translation: Unable to access gt-next configuration. Using defaults.";
 export declare const createNoEntryWarning: (id: string) => string;
