19622d0d4d142d24ec64c9dcc47ab3192a16536e
Brian Lou <brianlou2002@gmail.com>
2025-02-05 09:53:09 -0800

    Add optional field

--- packages/core/src/id/hashTemplate.ts
+++ packages/core/src/id/hashTemplate.ts
@@ -1,14 +1,11 @@
-import { hashString } from "./hashJsxChildren";
-import stringify from "fast-json-stable-stringify";
+import { hashString } from './hashJsxChildren';
+import stringify from 'fast-json-stable-stringify';
 
 export default function hashTemplate(
-    template: {
-        [key: string]: string
-    }
+  template: {
+    [key: string]: string;
+  },
+  hashFunction = hashString
 ): string {
-    return hashString(
-        stringify(
-            template
-        )
-    );
+  return hashFunction(stringify(template));
 }
