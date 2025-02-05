d634268c81cdc3b6c78312c34742b57029005e11
ErnestM1234 <ernestm@princeton.edu>
2025-02-04 21:33:03 -0800

    saving for the night coming back tmr

--- packages/react-cli/dist/fs/updateConfigFile.d.ts
+++ packages/react-cli/dist/fs/updateConfigFile.d.ts
@@ -1,5 +1,6 @@
 /**
  * Checks if the config file exists. If not, creates a new JSON file at the given filepath and writes the provided config object to it.
+ * If it does exist, add the version id
  * @param {string} configFilepath - The path to the config file.
  * @param {Record<string, any>} configObject - The config object to write if the file does not exist.
  */
--- packages/react-cli/src/fs/updateConfigFile.ts
+++ packages/react-cli/src/fs/updateConfigFile.ts
@@ -1,12 +1,11 @@
-import fs from "fs";
-import { displayCreatingNewConfigFile } from "../console/console";
+import fs from 'fs';
+import { displayCreatingNewConfigFile } from '../console/console';
 
-const INCLUDED_KEYS = [
-  "projectId", "defaultLocale", "_versionId"
-];
+const INCLUDED_KEYS = ['projectId', 'defaultLocale', '_versionId'];
 
 /**
  * Checks if the config file exists. If not, creates a new JSON file at the given filepath and writes the provided config object to it.
+ * If it does exist, add the version id
  * @param {string} configFilepath - The path to the config file.
  * @param {Record<string, any>} configObject - The config object to write if the file does not exist.
  */
@@ -14,19 +13,31 @@ export default function updateConfigFile(
   configFilepath: string,
   configObject: Record<string, any>
 ): void {
+  // Filter out empty string values from the config object
+  const filteredConfigObject = Object.fromEntries(
+    Object.entries(configObject).filter(
+      ([key, value]) => INCLUDED_KEYS.includes(key) && value !== ''
+    )
+  );
   try {
     // Check if the file exists
     if (!fs.existsSync(configFilepath)) {
-      // Filter out empty string values from the config object
-      const filteredConfigObject = Object.fromEntries(
-        Object.entries(configObject).filter(
-          ([key, value]) => INCLUDED_KEYS.includes(key) && value !== ""
-        )
-      );
       // Convert the config object to a JSON string
       const jsonContent = JSON.stringify(filteredConfigObject, null, 2);
       // Write the JSON string to the file, creating a new file if it doesn't exist
-      fs.writeFileSync(configFilepath, jsonContent, "utf-8");
+      fs.writeFileSync(configFilepath, jsonContent, 'utf-8');
+      // console.log
+      displayCreatingNewConfigFile(configFilepath);
+    } else {
+      const oldContent = JSON.parse(fs.readFileSync(configFilepath, 'utf-8'));
+      const newJsonContent = {
+        ...oldContent,
+        ...filteredConfigObject,
+      };
+      // Convert the config object to a JSON string
+      const jsonContent = JSON.stringify(newJsonContent, null, 2);
+      // Write the JSON string to the file, creating a new file if it doesn't exist
+      fs.writeFileSync(configFilepath, jsonContent, 'utf-8');
       // console.log
       displayCreatingNewConfigFile(configFilepath);
     }
