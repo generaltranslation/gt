d9c6d6a8a04d744dfec5ace7485abe3dbe15a816
ErnestM1234 <ernestm@princeton.edu>
2025-02-05 14:23:17 -0800

    build

--- packages/react-cli/dist/fs/config/updateConfigVersionId.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Update the config file version id and projectId (if necessary)
- * @param {Record<string, any>} configObject - The config object to write if the file does not exist.
- */
-export default function updateConfigVersionId(configFilepath: string, projectId?: string, _versionId?: string): void;
