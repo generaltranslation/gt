fe07808fd9daaaf8067040bbe86656a2fc53f6ac
Brian Lou <brianlou2002@gmail.com>
2025-02-12 15:23:08 -0800

    Run setup

--- examples/vite-create-app/src/App.tsx
+++ examples/vite-create-app/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { T, Var } from 'gt-react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <T id='app.0'>
+        <h1>Vite + React</h1>
+      </T>
+      <T id='app.1'>
+        <div className='card'>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is <Var>{count}</Var>
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+      </T>
+      <T id='app.2'>
+        <p className='read-the-docs'>
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </T>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
