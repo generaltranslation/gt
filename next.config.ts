f68bad99b25e27e2d91db482253fb005d4241959
Brian Lou <brianlou2002@gmail.com>
2025-02-17 16:43:40 -0800

    Squashed 'examples/next-gt-starter/' content from commit 989c19ee
    git-subtree-dir: examples/next-gt-starter
    git-subtree-split: 989c19ee03ada59ae7dfbaca22bdf631f4ce111d

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+import { initGT } from "gt-next/config";
+
+const nextConfig: NextConfig = initGT({})({
+  /* config options here */
+});
+
+export default nextConfig;
