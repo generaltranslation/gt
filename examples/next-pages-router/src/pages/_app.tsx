6bb223b5083197906a1351d6f1ee8f8eb482a691
Brian Lou <brianlou2002@gmail.com>
2025-02-18 15:27:49 -0800

    Update examples

--- examples/next-pages-router/src/pages/_app.tsx
+++ examples/next-pages-router/src/pages/_app.tsx
@@ -5,12 +5,7 @@ import config from '../../gt.config.json';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GTProvider
-      devApiKey={
-        'gtx-dev-4892a8c95c1a289811fe78268ab08f6e1f5834e3a44fad3a4be146eef1b052b7'
-      }
-      {...config}
-    >
+    <GTProvider {...config}>
       <Component {...pageProps} />
     </GTProvider>
   );
