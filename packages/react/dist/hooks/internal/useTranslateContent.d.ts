d299dc7bfdb44c9b17a95c7667b43bc20eb3edb1
ErnestM1234 <ernestm@princeton.edu>
2025-02-21 16:46:08 -0800

    build for alpha

--- packages/react/dist/hooks/internal/useTranslateContent.d.ts
+++
@@ -1,7 +0,0 @@
-import { TranslationOptions, TranslationsObject, RenderMethod } from '../../types/types';
-import { TranslateContentCallback } from '../../types/runtime';
-export default function useTranslateContent(translations: TranslationsObject | null, locale: string, defaultLocale: string, translationRequired: boolean, dialectTranslationRequired: boolean, runtimeTranslationEnabled: boolean, registerContentForTranslation: TranslateContentCallback, renderSettings: {
-    method: RenderMethod;
-    timeout?: number;
-}): (content: string, options?: TranslationOptions) => string;
-//# sourceMappingURL=useTranslateContent.d.ts.map
