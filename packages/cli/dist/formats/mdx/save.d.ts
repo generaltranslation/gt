2139915d5202513bdaf7e09bbd881b8c5a56558d
Brian Lou <brianlou2002@gmail.com>
2025-03-19 16:33:17 -0700

    Release alpha

--- packages/cli/dist/formats/mdx/save.d.ts
+++
@@ -1,5 +0,0 @@
-import { DataFormat } from '../../types/data';
-/**
- * Saves translated MDX/MD file content to the appropriate location
- */
-export declare function saveTranslatedFile(translatedContent: string, outputDir: string, fileName: string, dataFormat: DataFormat, locales: string[]): Promise<void>;
