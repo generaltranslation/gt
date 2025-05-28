939e2e144dabd292b9f08cd10e776afe78e19722
Brian Lou <brianlou2002@gmail.com>
2025-05-28 16:52:40 -0700

    Update code

--- packages/mcp/src/index.ts
+++ packages/mcp/src/index.ts
@@ -5,13 +5,14 @@ import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { addDocsTools } from './tools/docs.js';
 import { addDocsResource } from './resources/docs.js';
 import { readFileSync } from 'node:fs';
+import { fromPackageRoot } from './utils/getPaths.js';
 
 async function main() {
   const server = new McpServer({
     name: 'General Translation MCP Server',
-    version: JSON.parse(readFileSync('../package.json', 'utf8')).version,
+    version: JSON.parse(readFileSync(fromPackageRoot('package.json'), 'utf8'))
+      .version,
   });
-
   addDocsResource(server);
   addDocsTools(server);
   const transport = new StdioServerTransport();
--- packages/mcp/src/utils/getPaths.ts
+++ packages/mcp/src/utils/getPaths.ts
@@ -4,8 +4,5 @@ import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export function fromPackageRoot(relative: string) {
-  return path.resolve(__dirname, `../`, relative);
+  return path.resolve(__dirname, `../../`, relative);
 }
-
-// console.log() writes to stdout which will interfere with MCP Stdio protocol
-export const log = console.error;
