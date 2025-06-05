20b726b9d32769abb901d36f54c63c0db497f845
Archie McKenzie <90429137+archie-mckenzie@users.noreply.github.com>
2025-06-05 16:05:32 -0700

    Revert "a/custom locales" (#332)
    Revert "a/custom locales (#331)"
    
    This reverts commit 55c34d324b2a3a49c3678707142bd1cc975caef8.

--- packages/core/src/locales/getLocaleName.ts
+++ packages/core/src/locales/getLocaleName.ts
@@ -1,37 +1,25 @@
 import { intlCache } from '../cache/IntlCache';
 import { libraryDefaultLocale } from '../internal';
-import { getCustomProperty } from './customLocaleMapping';
-import { LocaleProperties } from './getLocaleProperties';
-import { _standardizeLocale } from './isValidLocale';
 
 /**
  * Retrieves the display name(s) of language code(s) using Intl.DisplayNames.
  *
- * @param {string} locale - A BCP-47 locale code.
+ * @param {string} code - A language code.
  * @param {string} [defaultLanguage=libraryDefaultLanguage] - The language for display names.
  * @returns {string} The display name(s) corresponding to the code(s), or empty string(s) if invalid.
  * @internal
  */
 export function _getLocaleName(
-  locale: string,
-  defaultLanguage: string = libraryDefaultLocale,
-  customMapping?: Record<string, string | LocaleProperties>
+  code: string,
+  defaultLanguage: string = libraryDefaultLocale
 ): string {
-  defaultLanguage ||= libraryDefaultLocale;
   try {
-    const standardizedLocale = _standardizeLocale(locale);
-    if (customMapping) {
-      for (const l of [locale, standardizedLocale, intlCache.get('Locale', locale).language]) {
-        const customName = getCustomProperty(customMapping, l, 'name');
-        if (customName) return customName;
-      }
-    }
     const displayNames = intlCache.get(
       'DisplayNames',
-      [defaultLanguage, standardizedLocale, libraryDefaultLocale], // default language order
+      [defaultLanguage, libraryDefaultLocale],
       { type: 'language' }
     );
-    return displayNames.of(standardizedLocale) || '';
+    return displayNames.of(code) || '';
   } catch {
     // In case Intl.DisplayNames construction fails, return empty string(s)
     return '';
