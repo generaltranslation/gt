16a4e575f6fcbdc3bbfb61198ce19ae8839c9bb0
ErnestM1234 <ernestm@princeton.edu>
2025-06-10 10:25:30 -0700

    fix: enforce that custom locale always comes first in middleware

--- packages/next/src/middleware-dir/utils.ts
+++ packages/next/src/middleware-dir/utils.ts
@@ -284,17 +284,22 @@ export async function getLocaleFromRequest(
   const { pathname } = req.nextUrl;
 
   // Custom getLocale
+  let addedCustomLocale = false;
   if (process.env._GENERALTRANSLATION_CUSTOM_GET_LOCALE_ENABLED === 'true') {
     try {
       const customRequestConfig = require('gt-next/_request');
       const customGetLocale: () => Promise<string> =
         customRequestConfig?.default || customRequestConfig.getLocale;
       const customLocale = await customGetLocale();
-      if (customLocale) candidates.push(customLocale);
+      if (customLocale) {
+        candidates.push(customLocale);
+        addedCustomLocale = true;
+      }
     } catch {
       /* empty */
     }
   }
+
   // Check pathname locales
   let pathnameLocale, unstandardizedPathnameLocale;
   if (localeRouting) {
@@ -334,7 +339,9 @@ export async function getLocaleFromRequest(
   if (cookieLocale?.value && isValidLocale(cookieLocale?.value)) {
     const resetCookie = req.cookies.get(resetLocaleCookieName);
     if (resetCookie?.value) {
-      candidates.unshift(cookieLocale.value);
+      addedCustomLocale
+        ? candidates.splice(1, 0, cookieLocale.value)
+        : candidates.unshift(cookieLocale.value);
       clearResetCookie = true;
     } else {
       candidates.push(cookieLocale.value);
