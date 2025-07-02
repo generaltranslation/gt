e9b4068cd9c324acb2c6c4a250d942748b6963b3
Brian Lou <brianlou2002@gmail.com>
2025-07-01 17:22:18 -0700

    Update locadex docs

--- packages/locadex/src/cli.ts
+++ packages/locadex/src/cli.ts
@@ -12,6 +12,8 @@ import { readFileSync } from 'node:fs';
 import { fromPackageRoot } from './utils/getPaths.js';
 import { main } from 'gtx-cli/index';
 import { GT_DASHBOARD_URL } from 'gtx-cli/utils/constants';
+import chalk from 'chalk';
+import { displayHeader, logMessage } from './logging/console.js';
 
 const packageJson = JSON.parse(
   readFileSync(fromPackageRoot('package.json'), 'utf8')
@@ -28,11 +30,18 @@ program
   .command('start')
   .description('Get started with Locadex')
   .action(async () => {
+    displayHeader();
+
     const urlToOpen = `${GT_DASHBOARD_URL}/locadex`;
     await import('open').then((open) =>
       open.default(urlToOpen, {
         wait: false,
       })
+    );
+    logMessage(
+      `${chalk.dim(
+        `If the browser window didn't open automatically, please open the following link:`
+      )}\n\n${chalk.cyan(urlToOpen)}`
     );
   });
 
