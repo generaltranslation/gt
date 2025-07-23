03b3367e98b155a21a723f0a645999f3efb40d18
Ernest McCarter <ernest@generaltranslation.com>
2025-07-23 14:10:42 -0700

    E/fix internal header (#490)
    * fix: internal headers
    
    * chore: changeset

--- packages/core/src/translate/utils/generateRequestHeaders.ts
+++ packages/core/src/translate/utils/generateRequestHeaders.ts
@@ -4,11 +4,18 @@ export default function generateRequestHeaders(
   config: TranslationRequestConfig,
   excludeContentType = false
 ) {
-  return {
+  const authHeaders: Record<string, string> = {
     ...(!excludeContentType && { 'Content-Type': 'application/json' }),
-    ...(config.apiKey && {
-      'x-gt-api-key': config.apiKey,
-    }),
     'x-gt-project-id': config.projectId,
   };
+
+  if (config.apiKey) {
+    if (config.apiKey.startsWith('gtx-internal-')) {
+      authHeaders['x-gt-internal-api-key'] = config.apiKey;
+    } else {
+      authHeaders['x-gt-api-key'] = config.apiKey;
+    }
+  }
+
+  return authHeaders;
 }
