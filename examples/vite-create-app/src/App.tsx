aecf4341df40060c17105c15c17caae086d940a9
ErnestM1234 <ernestm@princeton.edu>
2025-08-19 18:43:45 -0700

    fix: import

--- examples/vite-create-app/src/App.tsx
+++ examples/vite-create-app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { LocaleSelector, Num, T, useGT } from 'gt-react';
+import { LocaleSelector, Num, T } from 'gt-react';
 
 function App() {
   const [count, setCount] = useState(0);
