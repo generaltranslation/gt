706aa538005454290a5f0714e7f61715443b7849
fernando-aviles <fernando@generaltranslation.com>
2025-09-16 00:11:16 -0700

    F/stringify mdx plugin (#663)
    * localize import paths
    
    * return types
    
    * adding braces to escape
    
    * plugin
    
    * lint
    
    * remove rollup
    
    * readme
    
    * readme
    
    * changeset
    
    * tests
    
    * tests
    
    * readme
    
    * test
    
    * test

---
+++ packages/remark/src/index.ts
@@ -0,0 +1,46 @@
+import type { Plugin } from 'unified';
+import type { Root } from 'mdast';
+import { findAndReplace } from 'mdast-util-find-and-replace';
+
+const IGNORE_PARENTS = [
+  'code',
+  'inlineCode',
+  'mdxFlowExpression',
+  'mdxTextExpression',
+  'mdxjsEsm',
+  'yaml',
+  'toml',
+  'math',
+  'inlineMath',
+];
+
+// & that is NOT already an entity: &word;  &#123;  &#x1A2B;
+const AMP_NOT_ENTITY = /&(?![a-zA-Z][a-zA-Z0-9]*;|#\d+;|#x[0-9A-Fa-f]+;)/g;
+
+/**
+ * Escape HTML-sensitive characters ('{', '}', `&`, `<`, `>`, `"`, `'`) in text nodes,
+ * leaving code, math, MDX expressions, and front-matter untouched.
+ * Ensures literals render safely without altering already-escaped entities.
+ */
+const escapeHtmlInTextNodes: Plugin<[], Root> = function () {
+  return function (tree: Root) {
+    findAndReplace(
+      tree,
+      [
+        // Order matters: & first (idempotency), then the rest
+        [AMP_NOT_ENTITY, '&amp;'],
+        [/\{/g, '&#123;'],
+        [/\}/g, '&#125;'],
+        [/</g, '&lt;'],
+        [/>/g, '&gt;'],
+        [/"/g, '&quot;'],
+        [/'/g, '&#39;'],
+        [/_/g, '&#95;'],
+      ],
+      { ignore: IGNORE_PARENTS }
+    );
+  };
+};
+
+export default escapeHtmlInTextNodes;
+export { escapeHtmlInTextNodes };
