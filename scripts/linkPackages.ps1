033a4821d4fec5c6a633440386b1d2deb45e99cb
Brian Lou <brian@generaltranslation.com>
2025-09-29 17:01:23 -0700

    i hate windows

--- scripts/linkPackages.ps1
+++ scripts/linkPackages.ps1
@@ -6,18 +6,18 @@ Write-Output "Linking packages on Windows..."
 
 # Link packages to test-apps/next/base
 Set-Location test-apps/next/base
-pnpm link ../../../packages/next
-pnpm link ../../../packages/react
-pnpm link ../../../packages/core
-pnpm link ../../../packages/supported-locales
-Set-Location ../../..
+pnpm link ..\..\..\packages/next
+pnpm link ..\..\..\packages/react
+pnpm link ..\..\..\packages/core
+pnpm link ..\..\..\packages/supported-locales
+Set-Location ..\..\..
 
 # Link packages to test-apps/next/general-cases
 Set-Location test-apps/next/general-cases
-pnpm link ../../../packages/next
-pnpm link ../../../packages/react
-pnpm link ../../../packages/core
-pnpm link ../../../packages/supported-locales
-Set-Location ../../..
+pnpm link ..\..\..\packages/next
+pnpm link ..\..\..\packages/react
+pnpm link ..\..\..\packages/core
+pnpm link ..\..\..\packages/supported-locales
+Set-Location ..\..\..
 
 Write-Output "✅ Package linking completed on Windows"
