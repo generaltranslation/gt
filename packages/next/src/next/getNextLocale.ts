d0b7f5f59d3359269dddf8701b07173401a5e52c
Ernest McCarter <ernest@generaltranslation.com>
2025-10-01 11:14:50 -0700

    fix: ignore browser locale

--- packages/next/src/next/getNextLocale.ts
+++ packages/next/src/next/getNextLocale.ts
@@ -57,7 +57,7 @@ export async function getNextLocale(
     // }
 
     // Give an error here
-    if (preferredLocales.length === 0) {
+    if (preferredLocales.length === 0 && process.env._GENERALTRANSLATION_IGNORE_BROWSER_LOCALES === 'false') {
       throw new Error(noLocalesCouldBeDeterminedError);
     }
 
