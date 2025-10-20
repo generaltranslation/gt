c22dfcf8fd11e69d6f86873cc76cf82c0d69e366
Fernando Aviles <fernando@generaltranslation.com>
2025-10-20 10:53:23 -0700

    fix: lint

--- packages/cli/src/api/collectUserEditDiffs.ts
+++ packages/cli/src/api/collectUserEditDiffs.ts
@@ -112,7 +112,8 @@ export async function collectAndSendUserEditDiffs(
     // Helper to chunk array
     function chunk<T>(arr: T[], size: number): T[][] {
       const res: T[][] = [];
-      for (let i = 0; i < arr.length; i += size) res.push(arr.slice(i, i + size));
+      for (let i = 0; i < arr.length; i += size)
+        res.push(arr.slice(i, i + size));
       return res;
     }
 
