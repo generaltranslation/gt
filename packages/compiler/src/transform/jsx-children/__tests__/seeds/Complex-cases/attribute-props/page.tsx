ff4b36e692087c75148de3c8190a457a437aacc1
SamEggert <sam@generaltranslation.com>
2025-10-30 22:04:55 -0700

    update test to remove title from it

--- packages/compiler/src/transform/jsx-children/__tests__/seeds/Complex-cases/attribute-props/page.tsx
+++ packages/compiler/src/transform/jsx-children/__tests__/seeds/Complex-cases/attribute-props/page.tsx
@@ -1,22 +1,14 @@
-import { Branch, T } from "gt-next";
+import { Branch, T } from 'gt-next';
 
 export default function Home() {
   return (
-    <T
-      placeholder="Enter text"
-      title="Tooltip text"
-      alt="Alt text"
-    >
+    <T placeholder='Enter text' alt='Alt text'>
       <Branch
-        branch="element"
-        input={
-          <input placeholder="Branch placeholder" title="Branch title" />
-        }
-        image={
-          <img src="test.jpg" alt="Branch image" title="Branch image title" />
-        }
+        branch='element'
+        input={<input placeholder='Branch placeholder' />}
+        image={<img src='test.jpg' alt='Branch image' />}
         link={
-          <a href="#" aria-label="Branch link" aria-describedby="desc">
+          <a href='#' aria-label='Branch link' aria-describedby='desc'>
             Link
           </a>
         }
