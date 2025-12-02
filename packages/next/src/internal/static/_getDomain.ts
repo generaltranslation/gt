a39c4a22c240fb73cc6e8549e31fe3da5b8c9777
Ernest McCarter <ernest@generaltranslation.com>
2025-12-01 21:00:13 -0800

    chore: mark get requests function SSG fallbacks as deprecated

--- packages/next/src/internal/static/_getDomain.ts
+++ packages/next/src/internal/static/_getDomain.ts
@@ -1,5 +1,8 @@
 import { RequestFunctionReturnType } from '../../request/types';
 
+/**
+ * @deprecated
+ */
 export default async function getDomain(): Promise<RequestFunctionReturnType> {
   return undefined;
 }
