d71cb485a4eb8cd857ec08a6fb1da74c132a3adc
Ernest McCarter <ernest@generaltranslation.com>
2025-12-02 18:27:32 -0800

    fix: warning

--- packages/next/src/errors/cacheComponents.ts
+++ packages/next/src/errors/cacheComponents.ts
@@ -7,7 +7,7 @@ export const cacheComponentsMissingExperimentalLocaleResolutionWarning =
   'gt-next: cacheComponents is enabled, but experimentalLocaleResolution is not enabled. experimentalLocaleResolution must be enabled for i18n to work inside of cached components.';
 
 export const cacheComponentsExperimentalFeatureWarning =
-  'gt-next: You are using an experimental feature: experimentalLocaleResolution. This is a new feature and may be subject to change.';
+  'gt-next: You are using an experimental feature: experimentalLocaleResolution.';
 
 export const cacheComponentsExperimentalFeatureDisableGetRequestFunctionWarning =
   'gt-next: Because experimentalLocaleResolution is enabled, functions getRegion and getDomain are disabled.';
