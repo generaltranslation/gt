df5148e6963614c7533c4df704b4c608a0bd7fdb
Ernest McCarter <ernest@generaltranslation.com>
2025-12-02 17:22:09 -0800

    chore: more warnings and error checks

--- packages/next/src/errors/cacheComponents.ts
+++ packages/next/src/errors/cacheComponents.ts
@@ -1,2 +1,10 @@
+// ---- ERRORS ---- //
+export const cacheComponentSsgConflictError =
+  'gt-next Error: experimentalLocaleResolution and the deprecated experimentalEnableSSG are enabled. Only one of these can be enabled at a time.';
+
+// ---- WARNINGS ---- //
 export const cacheComponentsMissingExperimentalLocaleResolutionWarning =
   'gt-next: cacheComponents is enabled, but experimentalLocaleResolution is not enabled. experimentalLocaleResolution must be enabled for i18n to work inside of cached components.';
+
+export const cacheComponentsExperimentalFeatureWarning =
+  'gt-next: You are using an experimental feature: experimentalLocaleResolution. This is a new feature and may be subject to change.';
--- packages/next/src/plugin/checks/cacheComponentsChecks.ts
+++ packages/next/src/plugin/checks/cacheComponentsChecks.ts
@@ -1,14 +1,29 @@
 import { NextConfig } from 'next';
 import withGTConfigProps from '../../config-dir/props/withGTConfigProps';
-import { cacheComponentsMissingExperimentalLocaleResolutionWarning } from '../../errors/cacheComponents';
+import {
+  cacheComponentsExperimentalFeatureWarning,
+  cacheComponentsMissingExperimentalLocaleResolutionWarning,
+  cacheComponentSsgConflictError as cacheComponentLegacySsgConflictError,
+} from '../../errors/cacheComponents';
 
 export function cacheComponentsChecks(
   mergedConfig: withGTConfigProps,
   nextConfig: NextConfig
 ) {
-  if (!nextConfig.cacheComponents) return;
-
+  // checks for disabled experimentalLocaleResolution
   if (!mergedConfig.experimentalLocaleResolution) {
-    console.warn(cacheComponentsMissingExperimentalLocaleResolutionWarning);
+    if (nextConfig.cacheComponents) {
+      // Warn that i18n wont work inside of cached components
+      console.warn(cacheComponentsMissingExperimentalLocaleResolutionWarning);
+    }
+    return;
+  }
+
+  // Warn that this is an experimental feature
+  console.warn(cacheComponentsExperimentalFeatureWarning);
+
+  if (mergedConfig.experimentalEnableSSG) {
+    // Error if experimentalEnableSSG is enabled (conflicts, and we want to move people away from this legacy feature)
+    throw new Error(cacheComponentLegacySsgConflictError);
   }
 }
