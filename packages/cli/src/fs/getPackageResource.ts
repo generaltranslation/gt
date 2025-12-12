e407a8d77134c8b3d74256e136bcf5518030a861
Ernest McCarter <ernest@generaltranslation.com>
2025-12-11 17:39:42 -0800

    chore: debug

--- packages/cli/src/fs/getPackageResource.ts
+++ packages/cli/src/fs/getPackageResource.ts
@@ -1,12 +1,15 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { logger } from '../console/logger.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function fromPackageRoot(relative: string) {
+  logger.debug('fromPackageRoot __dirname' + __dirname);
   return path.resolve(__dirname, `../../`, relative);
 }
 
 export function fromBinariesRoot(relative: string) {
+  logger.debug('fromBinariesRoot __dirname' + __dirname);
   return path.resolve(__dirname, `../`, relative);
 }
