82ba9c9a0b398a4a5ae9f8139ad850f2d8e61aa6
Ernest McCarter <ernest@generaltranslation.com>
2026-02-04 20:03:20 -0800

    wip

--- packages/node/src/AsyncStorageAdapter.ts
+++ packages/node/src/AsyncStorageAdapter.ts
@@ -1,10 +1,18 @@
 import { StorageAdapter } from 'gt-i18n/internal';
 import { AsyncLocalStorage } from 'node:async_hooks';
 
+const asyncLocalStorage = new AsyncLocalStorage();
+
 export class AsyncStorageAdapter extends StorageAdapter {
-  getItem(key: string): string | undefined {}
+  getItem(key: string): string | undefined {
+    return asyncLocalStorage.getStore()?.get(key);
+  }
 
-  setItem(key: string, value: string): void {}
+  setItem(key: string, value: string): void {
+    asyncLocalStorage.getStore()?.set(key, value);
+  }
 
-  removeItem(key: string): void {}
+  removeItem(key: string): void {
+    asyncLocalStorage.getStore()?.delete(key);
+  }
 }
