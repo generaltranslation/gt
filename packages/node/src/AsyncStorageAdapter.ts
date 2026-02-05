7465e2e8c645197595c8a63d8acc1dfa6148eec1
Ernest McCarter <ernest@generaltranslation.com>
2026-02-04 19:44:23 -0800

    wip

--- packages/node/src/AsyncStorageAdapter.ts
+++ packages/node/src/AsyncStorageAdapter.ts
@@ -1,1 +1,10 @@
 import { StorageAdapter } from 'gt-i18n/internal';
+import { AsyncLocalStorage } from 'node:async_hooks';
+
+export class AsyncStorageAdapter extends StorageAdapter {
+  getItem(key: string): string | undefined {}
+
+  setItem(key: string, value: string): void {}
+
+  removeItem(key: string): void {}
+}
