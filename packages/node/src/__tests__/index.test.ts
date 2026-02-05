0267852fd532d1d2ab782d8f497f9d055d83b37c
Ernest McCarter <ernest@generaltranslation.com>
2026-02-05 13:29:41 -0800

    chore: satisfy greptile and ci

--- packages/node/src/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { helloWorld } from '../index';
-
-describe('gt-node', () => {
-  it('should return hello world', () => {
-    expect(helloWorld()).toBe('Hello World');
-  });
-});
