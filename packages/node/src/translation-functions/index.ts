7a9427e95bb0f76f0b261457e3ae37c1a80380bf
Ernest McCarter <ernest@generaltranslation.com>
2026-02-10 18:58:06 -0800

    chore: changeset

--- packages/node/src/translation-functions/index.ts
+++ packages/node/src/translation-functions/index.ts
@@ -1,12 +1,1 @@
-export {
-  gt,
-  m,
-  reg,
-  res,
-  msg,
-  decodeMsg,
-  decodeOptions,
-  declareVar,
-  declareStatic,
-  decodeVars,
-} from 'gt-i18n/internal';
+export { gt, m, reg, res } from 'gt-i18n/internal';
