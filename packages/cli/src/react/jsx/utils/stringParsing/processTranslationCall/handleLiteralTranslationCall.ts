c0cfb6d32476e49ba016c879fef2f0c4e79067c7
Ernest McCarter <ernest@generaltranslation.com>
2026-02-25 20:11:07 -0800

    fix: correct comment

--- packages/cli/src/react/jsx/utils/stringParsing/processTranslationCall/handleLiteralTranslationCall.ts
+++ packages/cli/src/react/jsx/utils/stringParsing/processTranslationCall/handleLiteralTranslationCall.ts
@@ -8,8 +8,7 @@ import { InlineMetadata } from './extractStringEntryMetadata.js';
 /**
  * For the processTranslationCall function, this function handles the case where a string literal or template literal is used.
  * @param arg - The argument to parse
- * @param options - The options to parse
- * @param tPath - The path to the argument
+ * @param metadata - The metadata to use
  * @param config - The configuration to use
  * @param output - The output to use
  * @param index - The index of the argument
