99fbdf77c38f7e177e014d943546920cf69ff4ee
moss <moss@generaltranslation.com>
2026-03-01 18:05:09 -0800

    feat: deprecate Static/declareStatic in favor of Derive/derive

--- packages/react-core-linter/src/utils/constants.ts
+++ packages/react-core-linter/src/utils/constants.ts
@@ -17,6 +17,7 @@ export const NUM_COMPONENT_NAME = 'Num';
 export const CURRENCY_COMPONENT_NAME = 'Currency';
 export const DATE_TIME_COMPONENT_NAME = 'DateTime';
 export const STATIC_COMPONENT_NAME = 'Static';
+export const DERIVE_COMPONENT_NAME = 'Derive';
 export const BRANCH_COMPONENT_NAME = 'Branch';
 export const PLURAL_COMPONENT_NAME = 'Plural';
 
@@ -29,6 +30,7 @@ export const USE_MESSAGES_FUNCTION_NAME = 'useMessages';
 export const GET_MESSAGES_FUNCTION_NAME = 'getMessages';
 export const MSG_FUNCTION_NAME = 'msg';
 export const DECLARE_STATIC_FUNCTION_NAME = 'declareStatic';
+export const DECLARE_DERIVE_FUNCTION_NAME = 'derive';
 
 export const GT_COMPONENT_NAMES = [
   T_COMPONENT_NAME,
@@ -37,6 +39,7 @@ export const GT_COMPONENT_NAMES = [
   CURRENCY_COMPONENT_NAME,
   DATE_TIME_COMPONENT_NAME,
   STATIC_COMPONENT_NAME,
+  DERIVE_COMPONENT_NAME,
 ];
 
 export const VARIABLE_COMPONENT_NAMES = [
--- packages/react-core/src/variables/Static.tsx
+++ packages/react-core/src/variables/Static.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
 
 /**
+ * `<Derive>` is a powerful but dangerous component which marks its children as statically analyzable for the compiler and CLI tool.
+ *
+ * This component is dangerous because it can cause the compiler and CLI tool to throw an error if children are not statically analyzable.
+ *
+ * @example
+ * ```jsx
+ * function getSubject() {
+ *   return (Math.random() > 0.5) ? "Alice" : "Brian";
+ * }
+ * ...
+ * <T>
+ *   <Derive>
+ *      {getSubject()}
+ *   </Derive>
+ *   is going to school today.
+ * </T>
+ * ```
+ *
+ * @param {T extends React.ReactNode} children - Static content to render.
+ * @returns {T} The result of the function invocation.
+ */
+export function Derive<T extends React.ReactNode>({
+  children,
+}: {
+  children: T;
+}): T {
+  return children;
+}
+
+/**
+ * @deprecated Use `<Derive>` instead.
+ *
  * `<Static>` is a powerful but dangerous component which marks its children as statically analyzable for the compiler and CLI tool.
  *
  * This component is dangerous because it can cause the compiler and CLI tool to throw an error if children are not statically analyzable.
