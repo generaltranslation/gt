29fd79703999fad89669825c511d9547191ece33
Ernest McCarter <ernest@generaltranslation.com>
2026-03-16 15:19:40 -0700

    add locale loader

--- packages/react/src/i18n-context/browser-i18n-manager/BrowserI18nManager.ts
+++ packages/react/src/i18n-context/browser-i18n-manager/BrowserI18nManager.ts
@@ -9,4 +9,13 @@ export class BrowserI18nManager extends I18nManager<BrowserStorageAdapter> {
   constructor(config: I18nManagerConstructorParams<BrowserStorageAdapter>) {
     super(config);
   }
+
+  /**
+   * Load translations for a given locale
+   * @param {string} locale - The locale to load translations for
+   * @returns {Promise<void>} A promise that resolves when the translations are loaded
+   */
+  async loadTranslations(locale: string = this.getLocale()): Promise<void> {
+    await this.getTranslations(locale);
+  }
 }
