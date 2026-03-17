ce4a1634b3ed48750af72e54584f5aeb405d6db3
Ernest McCarter <ernest@generaltranslation.com>
2026-03-16 17:55:13 -0700

    remove overwriting params

--- packages/react/src/i18n-context/setup/initializeGT.ts
+++ packages/react/src/i18n-context/setup/initializeGT.ts
@@ -2,7 +2,6 @@ import { setI18nManager } from 'gt-i18n/internal';
 import type { InitializeGTParams } from './types';
 import { BrowserStorageAdapter } from '../browser-i18n-manager/BrowserStorageAdapter';
 import { BrowserI18nManager } from '../browser-i18n-manager/BrowserI18nManager';
-import { libraryDefaultLocale } from 'generaltranslation/internal';
 
 /**
  * Configure GT for node runtime. This must be called to setup GT for node runtime.
@@ -11,11 +10,7 @@ import { libraryDefaultLocale } from 'generaltranslation/internal';
 export function initializeGT(params: InitializeGTParams): void {
   const i18nManager = new BrowserI18nManager({
     ...params,
-    storeAdapter: new BrowserStorageAdapter({
-      ...params,
-      defaultLocale: params.defaultLocale || libraryDefaultLocale,
-      locales: params.locales || [libraryDefaultLocale],
-    }),
+    storeAdapter: new BrowserStorageAdapter({ getLocale: params.getLocale }),
   });
   setI18nManager(i18nManager);
 }
