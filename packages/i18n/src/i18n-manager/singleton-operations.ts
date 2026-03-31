1d94b0ec07ed7a3c936c62de03665e1ce0810a60
Ernest McCarter <ernest@generaltranslation.com>
2026-03-31 12:41:36 -0700

    add generics to singleton-operations

--- packages/i18n/src/i18n-manager/singleton-operations.ts
+++ packages/i18n/src/i18n-manager/singleton-operations.ts
@@ -2,6 +2,7 @@ import { libraryDefaultLocale } from 'generaltranslation/internal';
 import { I18nManager } from './I18nManager';
 import logger from '../logs/logger';
 import { StorageAdapter } from './storage-adapter/StorageAdapter';
+import { Translation } from './translations-manager/utils/types/translation-data';
 
 // Singleton instance of I18nManager
 let i18nManager: I18nManager | undefined = undefined;
@@ -9,15 +10,18 @@ let i18nManager: I18nManager | undefined = undefined;
 /**
  * Get the singleton instance of I18nManager
  * @returns The singleton instance of I18nManager
+ * @template T - The type of the storage adapter
+ * @template U - The type of the translation that will be cached
  */
-export function getI18nManager<T extends StorageAdapter>():
-  | I18nManager<T>
-  | I18nManager<StorageAdapter> {
+export function getI18nManager<
+  T extends StorageAdapter = StorageAdapter,
+  U extends Translation = Translation,
+>(): I18nManager<T, U> | I18nManager<StorageAdapter, Translation> {
   if (!i18nManager) {
     logger.warn(
       'getI18nManager(): Translation failed because I18nManager not initialized.'
     );
-    i18nManager = new I18nManager({
+    i18nManager = new I18nManager<T, U>({
       defaultLocale: libraryDefaultLocale,
       locales: [libraryDefaultLocale],
     });
