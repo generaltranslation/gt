20a1efd692467253aee7869d626eaf87645d8ba2
moss-bryophyta <261561981+moss-bryophyta@users.noreply.github.com>
2026-03-31 10:32:24 -0700

    Address review feedback: fix magic strings, handle date prop in getVariableProps

--- packages/react-core/src/variables/_getVariableProps.ts
+++ packages/react-core/src/variables/_getVariableProps.ts
@@ -35,6 +35,7 @@ export default function getVariableProps(
     variableType: minifyVariableType(variableType),
     variableValue: (() => {
       if (typeof props.value !== 'undefined') return props.value;
+      if (typeof props.date !== 'undefined') return props.date;
       if (typeof props['data-_gt-unformatted-value'] !== 'undefined')
         return props['data-_gt-unformatted-value'];
       if (typeof props.children !== 'undefined') return props.children;
