54fbd45150120cc270307490fef4ad6d933f0b60
Ernest McCarter <ernest@generaltranslation.com>
2026-03-31 14:54:58 -0700

    export jsx translation function

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -2,4 +2,5 @@ export { t } from './translation-functions/t';
 export * from './translation-functions/msg';
 export * from './translation-functions/derive';
 export * from './translation-functions/fallbacks';
+export * from './translation-functions/jsx';
 export * from './helpers';
