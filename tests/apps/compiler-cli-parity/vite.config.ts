89db8875096c55168c377a4b23f1d2265649c7f9
Ernest McCarter <ernest@generaltranslation.com>
2026-04-02 16:49:20 -0700

    standardize tests to new logic

--- tests/apps/compiler-cli-parity/vite.config.ts
+++ tests/apps/compiler-cli-parity/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vite as gtPlugin } from '@generaltranslation/compiler';
+import gtConfig from './gt.config.json';
 
 export default defineConfig({
   plugins: [
     react(),
     gtPlugin({
-      enableAutoJsxInjection: true,
+      gtConfig,
       _debugHashManifest: true,
     }),
   ],
