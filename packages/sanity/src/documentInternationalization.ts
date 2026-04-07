b3b96f6aaef4c8ec9519cc1f3fa87c637d2f1f05
Brian Lou <brian@generaltranslation.com>
2026-04-06 20:47:57 -0700

    add

---
+++ packages/sanity/src/documentInternationalization.ts
@@ -0,0 +1,14 @@
+export {
+  documentInternationalization,
+  useDocumentInternationalizationContext,
+  DocumentInternationalizationMenu,
+  useDeleteTranslationAction,
+  useDuplicateWithTranslationsAction,
+} from '@sanity/document-internationalization';
+
+export type {
+  PluginConfig as DocumentInternationalizationConfig,
+  Language,
+  Metadata,
+  TranslationReference,
+} from '@sanity/document-internationalization';
