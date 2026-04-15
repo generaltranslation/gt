b2b73c2c648a83cc0512cbabb3b3e9b0b113cc97
Ernest McCarter <ernest@generaltranslation.com>
2026-04-15 14:27:07 -0700

    enforce icu

--- packages/react/src/i18n-context/functions/translation/GtInternalRuntimeTranslateString.ts
+++ packages/react/src/i18n-context/functions/translation/GtInternalRuntimeTranslateString.ts
@@ -3,6 +3,9 @@ import { tx } from 'gt-i18n/internal';
 /**
  * Internal runtime translate string function for dev only
  */
-const GtInternalRuntimeTranslateString = tx;
+const GtInternalRuntimeTranslateString: typeof tx = (content, options) => {
+  // Mark as ICU because that is the default format used for compiler collected translations (gt, t, msg)
+  return tx(content, { $format: 'ICU', ...options });
+};
 
 export { GtInternalRuntimeTranslateString };
