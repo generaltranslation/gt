18766a13bdabc58137564d96cb176ee57a9e9400
moss-bryophyta <261561981+moss-bryophyta@users.noreply.github.com>
2026-04-20 15:44:28 -0700

    style: fix prettier formatting for multi-arg constructors

--- packages/cli/src/workflows/enqueue.ts
+++ packages/cli/src/workflows/enqueue.ts
@@ -34,7 +34,12 @@ export async function runEnqueueWorkflow({
     // Create workflow with steps
     const branchStep = new BranchStep(gt, settings);
     // const queryFileDataStep = new QueryFileDataStep(gt);
-    const enqueueStep = new EnqueueStep(gt, settings, options.force, options.skipOnPaymentError);
+    const enqueueStep = new EnqueueStep(
+      gt,
+      settings,
+      options.force,
+      options.skipOnPaymentError
+    );
 
     // (1) run the branch step
     const branchData = await branchStep.run();
