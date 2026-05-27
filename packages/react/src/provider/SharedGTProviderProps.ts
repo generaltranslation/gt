f3efc40f59f372abf64127f729286cb20e344e5d
Ernest McCarter <ernest@generaltranslation.com>
2026-05-27 11:26:27 -0700

    fix: use condition store params in shared provider props

--- packages/react/src/provider/SharedGTProviderProps.ts
+++ packages/react/src/provider/SharedGTProviderProps.ts
@@ -1,12 +1,11 @@
 import type { InternalGTProviderProps } from '@generaltranslation/react-core/context';
-import type { LocaleCandidates } from 'gt-i18n/internal/types';
+import type { ReadonlyConditionStoreParams } from 'gt-i18n/internal/types';
 
 /**
  * We force the user to pass translations so they can be synchronously accessed
  *
  * - {@link InternalGTProviderProps} - requires translations and dictionaries
- * - locale - request/browser locale candidates
+ * - {@link ReadonlyConditionStoreParams} - requires locale
  */
-export type SharedGTProviderProps = InternalGTProviderProps & {
-  locale: LocaleCandidates;
-};
+export type SharedGTProviderProps = InternalGTProviderProps &
+  ReadonlyConditionStoreParams;
