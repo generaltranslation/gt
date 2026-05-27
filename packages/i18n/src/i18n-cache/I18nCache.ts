982ca2f501859a5c55a01bcd7faa5652942797dc
Ernest McCarter <ernest@generaltranslation.com>
2026-05-26 17:31:40 -0700

    refactor: replace cache locale consumers

--- packages/i18n/src/i18n-cache/I18nCache.ts
+++ packages/i18n/src/i18n-cache/I18nCache.ts
@@ -609,7 +609,8 @@ class I18nCache<
 
   private requiresTranslation(locale: string): boolean {
     return (
-      this.isTranslationEnabled() && getI18nConfig().requiresTranslation(locale)
+      this.isTranslationEnabled() &&
+      getI18nConfig().requiresTranslation(locale)
     );
   }
 
