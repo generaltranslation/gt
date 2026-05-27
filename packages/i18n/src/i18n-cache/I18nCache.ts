118fd5082ca948234ab41d7e011edc3050fa19a1
Ernest McCarter <ernest@generaltranslation.com>
2026-05-27 09:47:52 -0700

    style: format cache locale helper

--- packages/i18n/src/i18n-cache/I18nCache.ts
+++ packages/i18n/src/i18n-cache/I18nCache.ts
@@ -609,8 +609,7 @@ class I18nCache<
 
   private requiresTranslation(locale: string): boolean {
     return (
-      this.isTranslationEnabled() &&
-      getI18nConfig().requiresTranslation(locale)
+      this.isTranslationEnabled() && getI18nConfig().requiresTranslation(locale)
     );
   }
 
