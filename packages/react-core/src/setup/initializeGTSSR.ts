5e2c57db68e3077d7b80e313cc86dbfb60c6888d
Ernest McCarter <ernest@generaltranslation.com>
2026-05-29 16:15:29 -0700

    update init

--- packages/react-core/src/setup/initializeGTSSR.ts
+++ packages/react-core/src/setup/initializeGTSSR.ts
@@ -1,5 +1,4 @@
 import {
-  I18nCache,
   initializeI18nConfig,
   setupGTServicesEnabled,
 } from 'gt-i18n/internal';
@@ -7,26 +6,15 @@ import type {
   GTServicesEnabledParams,
   I18nConfigParams,
 } from 'gt-i18n/internal/types';
-import type { Translation } from 'gt-i18n/types';
-import type { ReactI18nCacheParams } from '../i18n-cache/ReactI18nCache';
 import { setRenderStrategy } from './globals';
-import { setReactI18nCache } from '../i18n-cache/singleton-operations';
 
 /**
- * Initialize GT for a server-side rendered application
- * - i18nCache
- *
- * ConditionStore and I18nStore are initialized in the provider at request time
- * TODO: auto detect if can find gt.config.json files
+ * Validation and setup for read only properties
  */
 export function internalInitializeGTSSR(
-  config: I18nConfigParams & GTServicesEnabledParams & ReactI18nCacheParams
+  config: I18nConfigParams & GTServicesEnabledParams
 ): void {
   setRenderStrategy('server-render');
-
   setupGTServicesEnabled(config);
   initializeI18nConfig(config);
-
-  const i18nCache = new I18nCache<Translation>(config);
-  setReactI18nCache(i18nCache);
 }
