d836e34aa7539efef9f727eda689ab607eeb4a08
Ernest McCarter <ernest@generaltranslation.com>
2026-06-16 16:35:02 -0700

    refactor: simplify condition store hooks

--- packages/react-core/src/hooks/condition-store.ts
+++ packages/react-core/src/hooks/condition-store.ts
@@ -1,11 +1,6 @@
 import { ReadonlyConditionStoreInterface } from 'gt-i18n/internal/types';
-import { useSyncExternalStore } from 'react';
 import { getReadonlyConditionStoreWithFallback } from '../condition-store/singleton-operations';
 import { useGTContext } from '../context/context';
-
-type SubscribableConditionStore = ReadonlyConditionStoreInterface & {
-  subscribe?: (listener: () => void) => () => void;
-};
 
 /**
  * NOTE: useSetLocale() and useSetEnableI18n() are not implemented in @generaltranslation/react-core
@@ -17,46 +12,23 @@ export function useConditionStore(): ReadonlyConditionStoreInterface {
   return context?.conditionStore ?? getReadonlyConditionStoreWithFallback();
 }
 
-function subscribeToConditionStore(
-  conditionStore: ReadonlyConditionStoreInterface,
-  listener: () => void
-): () => void {
-  return (
-    (conditionStore as SubscribableConditionStore).subscribe?.(listener) ??
-    (() => {})
-  );
-}
-
-function useConditionSnapshot<T>(
-  getSnapshot: (conditionStore: ReadonlyConditionStoreInterface) => T
-): T {
-  const conditionStore = useConditionStore();
-  return useSyncExternalStore(
-    (listener) => subscribeToConditionStore(conditionStore, listener),
-    () => getSnapshot(conditionStore),
-    () => getSnapshot(conditionStore)
-  );
-}
-
 /**
  * Returns the current locale.
  */
 export function useLocale(): string {
-  return useConditionSnapshot((conditionStore) => conditionStore.getLocale());
+  return useConditionStore().getLocale();
 }
 
 /**
  * Returns the current region, or undefined if no region is set.
  */
 export function useRegion(): string | undefined {
-  return useConditionSnapshot((conditionStore) => conditionStore.getRegion());
+  return useConditionStore().getRegion();
 }
 
 /**
  * Returns the current enableI18n flag.
  */
 export function useEnableI18n(): boolean {
-  return useConditionSnapshot((conditionStore) =>
-    conditionStore.getEnableI18n()
-  );
+  return useConditionStore().getEnableI18n();
 }
