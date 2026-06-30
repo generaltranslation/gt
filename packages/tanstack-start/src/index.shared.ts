c69824fd4bc135c2680f568a4723c53fbb977f58
Ben Gubler <git@bgub.dev>
2026-06-30 16:14:18 -0700

    refactor: consolidate tanstack start exports

---
+++ packages/tanstack-start/src/index.shared.ts
@@ -0,0 +1,42 @@
+export { parseLocale } from './functions/parseLocale';
+
+export {
+  // ===== Components ===== //
+  Branch,
+  Plural,
+  Derive,
+  LocaleSelector,
+  T,
+  Currency,
+  DateTime,
+  RelativeTime,
+  Var,
+  Num,
+  GTProvider,
+  // ===== Hooks ===== //
+  useLocale,
+  useSetLocale,
+  useCustomMapping,
+  useDefaultLocale,
+  useEnableI18n,
+  useSetEnableI18n,
+  useLocales,
+  useLocaleSelector,
+  useFormatLocales,
+  useGT,
+  useMessages,
+  useTranslations,
+  // ===== Functions ===== //
+  msg,
+  decodeMsg,
+  decodeOptions,
+  derive,
+  declareVar,
+  decodeVars,
+  mFallback,
+  gtFallback,
+  getTranslationsSnapshot,
+  t,
+  // ===== Setup ===== //
+  initializeGT,
+} from 'gt-react';
