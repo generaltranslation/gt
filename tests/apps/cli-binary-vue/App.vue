c31554c791c12c96080e9ffc6d7defe3ecc53df0
Ernest McCarter <ernest@generaltranslation.com>
2026-08-10 12:58:22 -0700

    test(cli): exercise Vue extraction in native binaries

---
+++ tests/apps/cli-binary-vue/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Branch, Plural, T } from 'gt-vue';
+</script>
+
+<template>
+  <T context="native Bun &amp; browser compiler">
+    <p title="Tea &amp; biscuits &notit">Rock&nbsp;&amp; roll &#x1f37d;</p>
+    <Plural
+      :n="1"
+      :zero="0"
+      :one="1"
+      :two="-1"
+      :few="3.14159"
+      :many="1e6"
+      :other="0xff"
+    />
+    <Branch
+      branch="status"
+      :active="true"
+      :inactive="false"
+      :unknown="null"
+      pending=""
+    />
+    <Plural
+      :n="1"
+      singular="Single 'quotes' inside"
+      plural='Double "quotes" inside'
+      other="Template with 'both' &quot;types&quot;"
+    />
+  </T>
+</template>
