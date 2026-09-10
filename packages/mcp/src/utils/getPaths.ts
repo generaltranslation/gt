aff68184cbaebf8b3e70da9be6059bb0c2c1175b
Brian Lou <brian@generaltranslation.com>
2026-09-09 18:19:54 -0700

    remove package

--- packages/mcp/src/utils/getPaths.ts
+++
@@ -1,8 +0,0 @@
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export function fromPackageRoot(relative: string) {
-  return path.resolve(__dirname, `../../`, relative);
-}
